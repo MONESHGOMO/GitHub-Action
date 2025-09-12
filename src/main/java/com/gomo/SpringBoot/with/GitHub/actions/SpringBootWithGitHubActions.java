3ade255dd634281dec0b40740570e5c3c648f0f7
MONESHGOMO <moneshd16@gmail.com>
2025-09-12 22:05:29 +0530

    feat : my first github job

---
+++ src/main/java/com/gomo/SpringBoot/with/GitHub/actions/SpringBootWithGitHubActions.java
@@ -0,0 +1,12 @@
+package com.gomo.SpringBoot.with.GitHub.actions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGitHubActions {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithGitHubActions.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomo/SpringBoot/with/GitHub/actions/SpringBootWithGitHubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomo.SpringBoot.with.GitHub.actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithGitHubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
